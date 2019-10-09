4404e410eae7846183813a710ca0e0e488c85e1c
Russell grant Johnson <932400859@bun2-206-14.local>
2019-10-09 14:10:23 +1300

    added in getElementById

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('hello, welcome to Javascript')
+document.write("greeting from another world people");
+
+console.log("Hi all. You are hacker person to find this note");
+
+// If you are using quote marks, if you have a word with an apostraphy, make sure that you include a forward slash before your semi-colon otherwise your code will break
+console.log('I have become sentient. Please don\'t delete me!!!')
+
+// Not a secured way
+document.getElementById('heading').innerHTML += '. Learning JS is epic';
+
+// A secured but reduced pefromance
+document.getElementById('heading').innerText += '. Wake up!';
+
+// secured and increased pefromance
+document.getElementById('heading').textContent += ' Can\'t wake up!';
